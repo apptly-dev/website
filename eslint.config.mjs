f6c56f02b3ce1c334611ecac1c26d7f956996dec
Alejandro Mery <amery@jpi.io>
2024-04-26 17:54:39 +0000

    add editorconfig file and delint
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   files: [
     '**/*.{cjs,mjs,js,ts,vue}',
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
+    'vue/multi-word-component-names': 0,
     '@stylistic/brace-style': ['error', '1tbs'],
     '@stylistic/indent': ['error', 2],
     '@stylistic/quotes': ['error', 'single'],
     '@stylistic/semi': ['error', 'always'],
   },
-})
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // isProduction = false
 const isProduction = process.env.PRODUCTION === 'production';
 // autoFix = true
-const autoFix = !isProduction &&
-  process.env.VITE_AUTO_FIX !== 'false' &&
-  process.env.VITE_AUTO_FIX !== 'no'
+const autoFix = !isProduction
+  && process.env.VITE_AUTO_FIX !== 'false'
+  && process.env.VITE_AUTO_FIX !== 'no';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src',
   devtools: { enabled: !isProduction },
 
-  extends: [ '@nuxt/ui-pro' ],
+  extends: ['@nuxt/ui-pro'],
 
   modules: [
     '@nuxt/eslint',
@@ -25,6 +25,6 @@ export default defineNuxtConfig({
     },
     config: {
       stylistic: true,
-    }
-  }
-})
+    },
+  },
+});

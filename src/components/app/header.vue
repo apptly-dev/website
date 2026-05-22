86293d060de6577c979cc37675f5ce688e1743e9
Nagy Károly Gábriel <k@apptly.co>
2026-05-22 16:54:43 +0300

    feat(header): make Apptly text a link to home
    Adds homeUrl to app.config.ts and wraps the header logo slot text in
    an anchor pointing to https://apptly.co.

--- src/components/app/header.vue
+++ src/components/app/header.vue
@@ -1,10 +1,14 @@
 <template>
   <p-header>
     <template #logo>
-      Apptly
+      <a :href="appConfig.homeUrl">Apptly</a>
     </template>
     <template #right>
       <p-color-mode-toggle />
     </template>
   </p-header>
 </template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
